b4870639c1d6b37b37b2a9176652c72e578e4e81
Suraj Yadav <sy083609@gmail.com>
2025-03-31 18:33:04 +0530

    updated commit

--- JavaScript/BOM.js
+++ JavaScript/BOM.js
@@ -6,8 +6,20 @@ function suraj(){
     // window.confirm("Are you ok");
     // window.prompt("enter your name");
 
-    let confirmans=window.confirm("Are you sure you want to delete this file?");
-    console.log(confirmans);
+    // let confirmans=window.confirm("Are you sure you want to delete this file?");
+    // console.log(confirmans);
+
+
+    // let promptmes=window.prompt("Enter your name");
+    // window.alert(promptmes)
+
+    let ans=confirm("Do you like my website?");
+    if(ans){
+        alert("Thank you for your feedback");
+    }
+    else{
+        alert("Sorry for the inconvenience");
+    }
 
 }
 suraj()
