5fca7a2c25ce0ab64ca79dc1b8ad2e709f04dcfa
Suraj Yadav <sy083609@gmail.com>
2025-04-01 14:16:34 +0530

    updated commit

---
+++ JavaScript/hw.js
@@ -0,0 +1,8 @@
+function suraj(){
+
+   let num1= window.prompt("Enter First Number:");
+    let num2=window.prompt("Enter Second Number:");
+    let sum=Number(num1)+Number(num2);
+    window.alert("The sum of two numbers is: "+sum);
+}
+suraj()
