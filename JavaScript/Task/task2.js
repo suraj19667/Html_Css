98c40dc1936ad25c6ea84dcf391011fc17aca522
Suraj Yadav <sy083609@gmail.com>
2025-04-01 14:32:06 +0530

    updated commit

---
+++ JavaScript/Task/task2.js
@@ -0,0 +1,4 @@
+let hasCompletedCourse = confirm("Have you completed the course?");
+let hasCertificate = confirm("Do you have a certificate?");
+
+hasCompletedCourse && hasCertificate ? alert("Completed") : alert("Not Completed");
