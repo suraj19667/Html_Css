43af5c352c2bd4db25c5bba758bdd01f385672a5
Suraj Yadav <sy083609@gmail.com>
2025-04-02 18:17:22 +0530

    updated commit

--- JavaScript/String.js
+++ JavaScript/String.js
@@ -1,2 +1,15 @@
-let a="This is string 1";
-let a2="This is string 2"
+// let a1="This is string 1";
+// let a2="This is string 2"
+
+// console.log(a1.length); //returns length of string
+
+// document.writeln(a1.concat(a2)); //add 2 or more strings
+
+let string="Cybrom"
+
+// toUpperCase=st.toUpperCase();
+// document.writeln(toUpperCase);
+// toLowerCase=st.toLowerCase();
+// document.writeln(toLowerCase);
+string.charCodeAt(2);
+document.writeln(string.charCodeAt(3)); 
