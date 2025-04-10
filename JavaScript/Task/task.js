06982dec55d086c403f3432ecea2337b908270ca
Suraj Yadav <sy083609@gmail.com>
2025-04-10 18:20:12 +0530

    updated commit

---
+++ JavaScript/Task/task.js
@@ -0,0 +1,16 @@
+
+// let change=() => {
+//     let ptag=document.querySelector("#suraj")
+
+//     ptag.style.fontSize="20px"
+// }
+let hide=() => {
+    let text=document.querySelector("#suraj")
+
+    text.style.display="none";
+}
+let show=() => {
+    let text=document.querySelector("#suraj")
+
+    text.style.display="block";
+}
