88a563c86f46a2a88f5e87e64a8f11efa753985b
Suraj Yadav <sy083609@gmail.com>
2025-04-11 09:46:16 +0530

    updated commit

--- JavaScript/Task/task.js
+++ JavaScript/Task/task.js
@@ -4,13 +4,25 @@
 
 //     ptag.style.fontSize="20px"
 // }
-let hide=() => {
-    let text=document.querySelector("#suraj")
+// let hide=() => {
+//     let text=document.querySelector("#suraj")
 
-    text.style.display="none";
-}
-let show=() => {
-    let text=document.querySelector("#suraj")
+//     text.style.display="none";
+// }
+// let show=() => {
+//     let text=document.querySelector("#suraj")
 
-    text.style.display="block";
-}
+//     text.style.display="block";
+// }
+
+
+    let count = 0;
+
+    const button = document.getElementById('clickButton');
+    const countDisplay = document.getElementById('countDisplay');
+
+    button.addEventListener('click', function() {
+        count++;
+        countDisplay.textContent = 'Count: ' + count;
+    });
+
