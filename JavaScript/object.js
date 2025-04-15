18ccd69053123a61182417a09fe64a79b42eecb1
Suraj Yadav <sy083609@gmail.com>
2025-04-15 17:57:13 +0530

    objects

---
+++ JavaScript/object.js
@@ -0,0 +1,16 @@
+let Fruits=["suraj",25,"bhopal"]//Array
+
+let obj={
+//key:value
+Name:"suraj",
+Age:25,
+City:"bhopal",
+//Age:20 key should be unique
+}
+// console.log(obj);//to print full object
+// console.log(obj.Age); //To access the value of key Age
+// obj.College="Cybrom";
+// obj.Age=26;
+delete obj.Age; //To delete the key Age
+
+console.log(obj);
